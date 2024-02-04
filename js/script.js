83bda322a9cbdeb5d2e4c5a465c310e516e77dd2
Yurii Nikitin <ykypika@gmail.com>
2024-02-05 01:35:52 +0300

    add bootstrap 5.2.3

--- js/script.js
+++ js/script.js
@@ -7,54 +7,55 @@ import { getYear, getMonth } from "./get-month-get-year.js";
 
 import renderSettingsMenu from "./render/render-settings-menu.js";
 
-const graphic = document.getElementById("graphic");
 
+document.addEventListener("DOMContentLoaded", () => {
 
+    const graphic = document.getElementById("graphic");
 
-
-renderGraphicValues();
-renderCalendar(new Date().getFullYear(), new Date().getMonth(), graphic.value);
-renderStatistics();
-
-// renderSettingsMenu();
-
-
-graphic.addEventListener("change", (e) => {
-    renderCalendar(getYear(), getMonth(), e.target.value);
+    renderGraphicValues();
+    renderCalendar(new Date().getFullYear(), new Date().getMonth(), graphic.value);
     renderStatistics();
-    localStorage.setItem("current_graphic", e.target.value);
-});
-
-
-function updateCalendar() {
-
-    const now = new Date();
-    const tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1,);
-    const timeUntilTomorrow = tomorrow - now;
-
     
-    setTimeout(() => {
-        const currentYear = now.getFullYear();
-        const currentMonth = now.getMonth();
-        const calendarMonth = getMonth();
-        const calendarYear = getYear();
-
-        if(currentMonth === calendarMonth && currentYear === calendarYear) {
-            renderCalendar(new Date().getFullYear(),  new Date().getMonth(), graphic.value);
-            console.log("Updated!");
-        }
-        updateCalendar();
-    }, timeUntilTomorrow);
-}
-
-
-
-// window.addEventListener("focus", () => {
-//     const calendarCurrentDay = +document.querySelector(".today").querySelector("div").innerHTML;
-//     const currentDay = new Date().getDate();
-
-//     console.log(calendarCurrentDay)
-
-// });
-
-updateCalendar();
+    // renderSettingsMenu();
+    
+    
+    graphic.addEventListener("change", (e) => {
+        renderCalendar(getYear(), getMonth(), e.target.value);
+        renderStatistics();
+        localStorage.setItem("current_graphic", e.target.value);
+    });
+    
+    
+    function updateCalendar() {
+    
+        const now = new Date();
+        const tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1,);
+        const timeUntilTomorrow = tomorrow - now;
+    
+        
+        setTimeout(() => {
+            const currentYear = now.getFullYear();
+            const currentMonth = now.getMonth();
+            const calendarMonth = getMonth();
+            const calendarYear = getYear();
+    
+            if(currentMonth === calendarMonth && currentYear === calendarYear) {
+                renderCalendar(new Date().getFullYear(),  new Date().getMonth(), graphic.value);
+                console.log("Updated!");
+            }
+            updateCalendar();
+        }, timeUntilTomorrow);
+    }
+    
+    
+    
+    // window.addEventListener("focus", () => {
+    //     const calendarCurrentDay = +document.querySelector(".today").querySelector("div").innerHTML;
+    //     const currentDay = new Date().getDate();
+    
+    //     console.log(calendarCurrentDay)
+    
+    // });
+    
+    updateCalendar();
+  });
