1713faf29383eb4bc86759edff2564f52b710568
Iurii Nikitin <dev.iuriinikitin@gmail.com>
2024-02-08 19:55:25 +0300

    add bootstrap styles

--- js/get-table-header.js
+++ js/get-table-header.js
@@ -7,8 +7,8 @@ export default function getTableHeader(month, year, colspan = [0, 0, 0]) {
     return `<thead>
     <tr>
         <th colspan="${colspan[0]}">
-            <button type="button" class="btn btn-light">
-                <img class="arrow d-block" src="./img/left.svg" alt="left">
+            <button type="button" class="btn btn-light arrow-left">
+                <img class="d-block" src="./img/left.svg" alt="left">
             </button>
         </th>
         <th colspan="${colspan[1]}">
@@ -16,8 +16,8 @@ export default function getTableHeader(month, year, colspan = [0, 0, 0]) {
             <button class="btn btn-primary btn-sm btn-year">${year}</button>
         </th>
         <th colspan="${colspan[2]}">
-            <button type="button" class="btn btn-light">
-                <img class="arrow d-block" src="./img/right.svg" alt="right">
+            <button type="button" class="btn btn-light arrow-right">
+                <img class="d-block" src="./img/right.svg" alt="right">
             </button>
         </th>
     </tr>`;
--- js/render/render-calendar.js
+++ js/render/render-calendar.js
@@ -34,8 +34,8 @@ export default function renderCalendar(year, month, graphic, id = "calendar") {
     for(let i = 0; i < days.length; i++) {
 
         let clazz = days[i].actualType;
-        let iconsTop = "<div class='position-absolute top-0 start-0 mt-1 ms-1 h-17 d-flex icons_top'>";
-        let iconsBottom = "<div class='position-absolute bottom-0 start-0 mb-1 ms-1 h-17 d-flex icons_bottom'>";
+        let iconsTop = "<div class='order-1 d-flex justify-content-between h-17 day__header'><div class='d-flex icons'>";
+        let iconsBottom = "<div class='order-3 d-flex h-17 day__footer'>";
         const num = days[i].date.getDate();
         const name = days[i].name;
         if(days[i].today) {clazz += " today"};
@@ -44,12 +44,14 @@ export default function renderCalendar(year, month, graphic, id = "calendar") {
         if(days[i].note) {iconsBottom += "<img class='d-block h-100 me-1' src='./img/note.svg' alt='note'>"};
         if(days[i].timeChanged) {iconsBottom += "<img class='d-block h-100 me-1' src='./img/clock.svg' alt='clock'>"};
 
-        calendar += `<td class="position-relative border day ${clazz}">
-                        <div class="position-absolute top-0 end-0 me-1">${num}</div>
-                        <div>${name}</div>`;
+        calendar += `<td class="border p-1 p-sm-2 day ${clazz}">
+												<div class="d-flex flex-column justify-content-between h-100">
+													<div class="order-2">${name}</div>
+												
+												`;
 
-        if(days[i].salary || days[i].holiday) {calendar += iconsTop + "</div>"};
-        if (days[i].note || days[i].timeChanged) {calendar += iconsBottom};
+        calendar += iconsTop + `</div><div class="lh-1">${num}</div></div>`;
+        calendar += iconsBottom;
         
         if(days[i].dayWeek === 7) {calendar += "<tr>"}
 
@@ -57,12 +59,12 @@ export default function renderCalendar(year, month, graphic, id = "calendar") {
 
     document.getElementById(id).innerHTML += calendar;
 
-    document.getElementById(id).querySelectorAll(".arrow")[0].addEventListener("click", () => {
+    document.getElementById(id).querySelector(".arrow-left").addEventListener("click", () => {
         renderCalendar(+year, month - 1, graphic);
         renderStatistics();
 
     });
-    document.getElementById(id).querySelectorAll(".arrow")[1].addEventListener("click", () => {
+    document.getElementById(id).querySelector(".arrow-right").addEventListener("click", () => {
         renderCalendar(+year, month + 1, graphic);
         renderStatistics();
     });
